423beb9a6bc09414b702d8aa66516fbdc51c049f
AndreyB <zoy788@gmail.com>
2021-09-26 14:37:16 +0300

    CZhanges to be committed:

---
+++ djangoProject5/News_Portal/templatetags/custom_filters.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+censor_list = ['слово1', 'слово2', 'слово3']
+value_new = []
+
+@register.filter(name='Censor')
+def censor(value):
+    for word in censor_list:
+        value = value.replace(word, '*****')
+    return value
+
+
+
